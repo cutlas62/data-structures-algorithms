099cc044138540f4ed743071dde49f1efb485917
Carlos <carlos.santosr@ieee.org>
2020-06-20 15:45:02 -0700

    Added simple BFS

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <stack>
-#include <set>
+#include <unordered_set>
 #include <deque>
 
 using namespace std;
@@ -21,29 +21,72 @@ void printDFS (unordered_map<string, vector<string>> graph, string startNode)
 
     stack<string> nodesToVisit;
     nodesToVisit.push(startNode);
-    set<string> visitedNodes;
+    unordered_set<string> visitedNodes;
 
     while (!nodesToVisit.empty())
     {
         string curNode = nodesToVisit.top();
         nodesToVisit.pop();
+
+        if (visitedNodes.count(curNode))
+        {
+            continue;
+        }
+        
         visitedNodes.insert(curNode);
         printf("%s ", curNode.c_str());
 
         for (string neighbor : graph.find(curNode)->second)
         {
-            if (!visitedNodes.count(neighbor))
+            if (visitedNodes.find(neighbor) == visitedNodes.end())
             {
                 nodesToVisit.push(neighbor);
             }
         }
     }
+    printf("\n%ld nodes\n", visitedNodes.size());
     printf("\n");
 }
 
 
 
+void printBFS (unordered_map<string, vector<string>> graph, string startNode)
+{
+    printf("\nBFS\n");
+    if (graph.count(startNode) == 0)
+    {
+        throw invalid_argument("startNode is not in the graph");
+    }
+    int nNodes = 0;
 
+    deque<string> nodesToVisit;
+    nodesToVisit.push_back(startNode);
+    unordered_set<string> visitedNodes;
+
+    while (!nodesToVisit.empty())
+    {
+        string curNode = nodesToVisit.front();
+        nodesToVisit.pop_front();
+
+        if(visitedNodes.count(curNode)){
+        	continue;
+        }
+
+        visitedNodes.insert(curNode);
+        printf("%s ", curNode.c_str());
+        nNodes++;
+
+        for (string neighbor : graph.find(curNode)->second)
+        {
+            if (!visitedNodes.count(neighbor))
+            {
+                nodesToVisit.push_back(neighbor);
+            }
+        }
+    }
+    printf("\n%ld nodes\n", visitedNodes.size());
+    printf("\n");
+}
 
 
 int main (int argc, char *argv [])
@@ -72,7 +115,7 @@ int main (int argc, char *argv [])
         {"Valencia", {"Castellon", "Requena", "Albacete", "Alicante"}},
         {"Castellon", {"Valencia"}},
         {"Albacete", {"Cuenca", "Valencia", "Alicante"}},
-        {"Alicante", {"Valencia", "Requena", "Albacete"}},
+        {"Alicante", {"Valencia", "Requena", "Albacete", "Almeria"}},
         {"Puertollano", {"Madrid", "Cordoba"}},
         {"Cordoba", {"Puertollano", "Sevilla", "Malaga", "Granada"}},
         {"Sevilla", {"Cordoba", "Cadiz", "Malaga"}},
@@ -84,5 +127,11 @@ int main (int argc, char *argv [])
 
 
     printDFS(unweightedGraph, "Madrid");
+    //printDFS(unweightedGraph, "Requena");
+    //printDFS(unweightedGraph, "Sevilla");
+
+    printBFS(unweightedGraph, "Madrid");
+    //printBFS(unweightedGraph, "Requena");
+    //printBFS(unweightedGraph, "Sevilla");
 
 }
