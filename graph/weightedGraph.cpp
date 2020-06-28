73497846175f0ffae9bd6c57b8eb1074091066a0
Carlos <carlos.santosr@ieee.org>
2020-06-27 23:34:56 -0700

    Added Kruskal's MCST

--- graph/weightedGraph.cpp
+++ graph/weightedGraph.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <limits>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <stack>
@@ -87,10 +88,11 @@ void printDFS(vector<string> &vertices, double matrix [29][29], string startNode
 void printPrimMCST (vector<string> &vertices, double matrix[29][29])
 {
 
+    double mcstCost = 0;
+
     unordered_set<string> nodesIncludedInMCST;
     unordered_map<string, pair<string, double>> edgeCosts;	// <child, <parent, cost>>
     unordered_map<string, vector<string>> mcst;
-    double mcstCost = 0;
 
     for (string curVertex : vertices)
     {
@@ -184,6 +186,120 @@ void printPrimMCST (vector<string> &vertices, double matrix[29][29])
 
 }
 
+// Standard DFS
+bool formsCycle (unordered_map<string, vector<string>> &graph, string startVertex, string endVertex)
+{
+    if(graph.find(startVertex) == graph.end() || graph.find(endVertex) == graph.end())
+    {
+        return false;
+    }
+
+    stack<string> verticesToVisit;
+    verticesToVisit.push(startVertex);
+    unordered_set<string> visitedVertices;
+
+    while (!verticesToVisit.empty())
+    {
+        string curVertex = verticesToVisit.top();
+        verticesToVisit.pop();
+        visitedVertices.insert(curVertex);
+        if(curVertex == endVertex)
+        {
+            return true;
+        }
+
+        vector<string> neighbors = graph.find(curVertex)->second;
+        for (string curNeighbor : neighbors)
+        {
+            if (visitedVertices.find(curNeighbor) == visitedVertices.end())
+            {
+                verticesToVisit.push(curNeighbor);
+            }
+        }
+
+    }
+    return false;
+
+}
+
+void printKruskalMCST (vector<string> &vertices, double matrix[29][29])
+{
+    double mcstCost = 0;
+    int mcstNEdges = 0;
+    int nVertices = vertices.size();
+    unordered_map<string, vector<string>> mcst;
+
+    // Create a vector with all the edges
+    vector<pair<double, pair<string, string>>> edges;
+    for(int i = 0; i < 29; i++)
+    {
+        for(int j = i; j < 29; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                edges.push_back(make_pair(matrix[i][j], make_pair(vertices[i], vertices[j])));
+            }
+        }
+    }
+
+    // Sort this vector in ascending order, O(n lgn)
+    sort(edges.begin(), edges.end());
+
+    // Kruskal's
+    vector<pair<double, pair<string, string>>>::iterator it = edges.begin();
+    while (mcstNEdges < nVertices - 1)
+    {
+
+        double curWeight = it->first;
+        string curVertex1 = it->second.first;
+        string curVertex2 = it->second.second;
+
+        if (!formsCycle(mcst, curVertex1, curVertex2))
+        {
+            if(mcst.find(curVertex1) == mcst.end())
+            {
+                mcst.insert(make_pair(curVertex1, vector<string>()));
+            }
+            mcst.find(curVertex1)->second.push_back(curVertex2);
+
+            if(mcst.find(curVertex2) == mcst.end())
+            {
+                mcst.insert(make_pair(curVertex2, vector<string>()));
+            }
+            mcst.find(curVertex2)->second.push_back(curVertex1);
+
+
+            mcstCost += curWeight;
+            mcstNEdges++;
+        }
+
+        it++;
+    }
+
+
+    // Print the mcst graph
+    printf("\nKruskal's MCST cost = %.1f\n", mcstCost);
+    unordered_map<string, vector<string>>::iterator mcst_it;
+    for (mcst_it = mcst.begin(); mcst_it != mcst.end(); mcst_it++)
+    {
+        string curNode = mcst_it->first;
+        printf("%s\n", curNode.c_str());
+
+        vector<string> neighbors = mcst_it->second;
+        if (!neighbors.empty())
+        {
+            printf("\t");
+        }
+        for(string curNeighbor : neighbors)
+        {
+            printf("%s ", curNeighbor.c_str());
+        }
+        printf("\n");
+    }
+
+}
+
+
 
 double calculateGraphCost(double matrix[29][29])
 {
@@ -242,9 +358,11 @@ int main (int argc, char *argv [])
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 291.6, 0, 0, 0, 0, 0, 167.6, 0}     			// Almeria
     };
 
-    printDFS(vertices, adjacencyMatrixGraph, "Madrid");
+    //printDFS(vertices, adjacencyMatrixGraph, "Madrid");
 
-    printf("\nInitial graph cost = %.1f\n", calculateGraphCost(adjacencyMatrixGraph));
-    printPrimMCST(vertices, adjacencyMatrixGraph);
+    //printf("\nInitial graph cost = %.1f\n", calculateGraphCost(adjacencyMatrixGraph));
+    //printPrimMCST(vertices, adjacencyMatrixGraph);
+
+    printKruskalMCST(vertices, adjacencyMatrixGraph);
 
 }
